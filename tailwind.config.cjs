393bde98508896b5e7081ca5184dbf51a96cbd1d
Kamilla Huhnke <kaan0001@stud.kea.dk>
2024-03-22 16:00:14 +0100

    knapper

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-  };
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        rod: "#FF1054",
+        hoverrod: "#FF5C8B",
+        clickrod: "#BE1343",
+        gron: "#007C7A",
+        hovergron: "#009F9C",
+        clickgron: "#006261",
+      },
+      fontFamily: {},
+    },
+  },
+};
